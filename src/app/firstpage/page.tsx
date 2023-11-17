e46d054edf7dfb00760ba02e1281fcc9d88f5f0c
Ali Cagatay <aliccagatay@gmail.com>
2023-11-17 18:34:21 +0000

    Add button to go back to the `/` route.

--- src/app/firstpage/page.tsx
+++ src/app/firstpage/page.tsx
@@ -1,8 +1,10 @@
+import Link from "next/link";
 import styles from "./styles.module.css";
 const FirstPage = () => {
   return (
     <div className={styles.main}>
       <h1>First Page</h1>
+      <Link href="/">Go back to the home page</Link>
     </div>
   );
 };
