c4496c08207346a8373cd61de299deae950e63e8
Ali Cagatay <aliccagatay@gmail.com>
2023-11-17 18:02:12 +0000

    Updated `page.tsx` for the `firstpage` route

---
+++ src/app/firstpage/page.tsx
@@ -0,0 +1,10 @@
+import styles from "./styles.module.css";
+const FirstPage = () => {
+  return (
+    <div className={styles.main}>
+      <h1>First Page</h1>
+    </div>
+  );
+};
+
+export default FirstPage;
