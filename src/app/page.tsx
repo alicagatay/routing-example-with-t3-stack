94cbd9ad96ccbc5bc7851b1ba8d2e817f9347840
Ali Cagatay <aliccagatay@gmail.com>
2023-11-17 18:34:02 +0000

    Add button to go to the `firstpage` route.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
+import Link from "next/link";
 import styles from "./index.module.css";
 const HomePage = () => {
   return (
     <div className={styles.main}>
       <h1>Home Page</h1>
+      <Link href="/firstpage">Go to the first page</Link>
     </div>
   );
 };
